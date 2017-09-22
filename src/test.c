f2e0d56f5afffcf36db135e10224fb6551b60fa6
EdwinRybarczyk <edwinrybarczyk4d@gmail.com>
2017-09-22 22:50:49 +0100

    window init

--- src/test.c
+++ src/test.c
@@ -5,12 +5,44 @@
 
 int main()
 {
+	char* title = "title";
+	int width = 500;
+	int height = 500;
+
+	//SDL init
 	if(SDL_Init(SDL_INIT_VIDEO) != 0){printf("Couldnt initialise SDL!");return -1;}
+	
+	SDL_Window* window = SDL_CreateWindow(
+		title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		width, height, SDL_WINDOW_SHOWN);
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	
+	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888,
+		SDL_TEXTUREACCESS_STREAMING, width, height);
+
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_Event event;
+
+	//Renderer init
+	Screen* screen = createScreen(width,height);	
+	Rasterizer* rasterizer = createRasterizer(screen);
 
 	char running = 1;
 	while(running)
 	{
-		break;
+		while(SDL_PollEvent(&event)){
+			if(event.type == SDL_QUIT){running = 0; break;}}
+
+		//Do rendering here
+
+	
+		//Stop rendering
+		SDL_UpdateTexture(texture,NULL, screen->pixels + 0, width * 4);
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
+		SDL_RenderPresent(renderer);
+
+		SDL_RenderClear(renderer);
 	}
 	
 
